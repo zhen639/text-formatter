83c75b1b7e8c3e7b2766e6e673518aa6659c608e
zhen639 <zhen6939@gmail.com>
2016-08-01 19:43:07 +0800

    break down string by width

--- spec/text_formatter_spec.rb
+++ spec/text_formatter_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 
 RSpec.describe TextFormatter do
   describe '#soft_return' do
-    it 'should lines whose width is set' do
-      expect(true).to eq(true)
+    it 'should break down text into lines by width' do
+      result = TextFormatter.soft_return(text, 10)
+      expect(result).to eq("These is\nan apple I\nwant to\neat!\n")
+
+      result = TextFormatter.soft_return(text, 5)
+      expect(result).to eq("These\nis an\napple\nI\nwant\nto\neat!\n")
     end
   end
+
+  let(:text) { 'These is an apple I want to eat!'}
 end
--- src/text_formatter.rb
+++ src/text_formatter.rb
@@ -1,5 +1,5 @@
 module TextFormatter
-  def self.soft_return(text, column_width)
-
+  def self.soft_return(text, width)
+    text.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
   end
 end
